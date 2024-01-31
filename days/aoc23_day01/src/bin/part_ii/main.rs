c6546365718eecc13ad62fc932cf460ca699337d
Cheng Che <clementcheng56@gmail.com>
2024-01-31 18:09:25 +0800

    refactor(aoc23_day01): auto format with rustfmt

--- days/aoc23_day01/src/bin/part_ii/main.rs
+++ days/aoc23_day01/src/bin/part_ii/main.rs
@@ -1,6 +1,9 @@
 fn main() {
     let input = include_str!("./test_data/input.txt");
-    println!("Sum of calibration numbers: {}", sum_numeric_calibration_nums(input));
+    println!(
+        "Sum of calibration numbers: {}",
+        sum_numeric_calibration_nums(input)
+    );
 }
 
 trait Numeric {
@@ -8,7 +11,7 @@ trait Numeric {
 }
 
 impl Numeric for &str {
-    fn to_digit(&self) -> Option<u32>{
+    fn to_digit(&self) -> Option<u32> {
         match *self {
             "one" => Some(1),
             "two" => Some(2),
@@ -19,7 +22,7 @@ impl Numeric for &str {
             "seven" => Some(7),
             "eight" => Some(8),
             "nine" => Some(9),
-            _ => None
+            _ => None,
         }
     }
 }
@@ -39,19 +42,26 @@ fn find_numeric_digit(text: &str, forward: bool) -> Option<u32> {
     let len = chars.len();
 
     for i in 0..len {
-        let c = if forward { chars[i] } else { chars[len - i - 1] };
-        if let Some(digit) = c.to_digit(10) { return Some(digit) }
+        let c = if forward {
+            chars[i]
+        } else {
+            chars[len - i - 1]
+        };
+        if let Some(digit) = c.to_digit(10) {
+            return Some(digit);
+        }
         for digits in 3..=5 {
             if i + digits < len {
                 let start = if forward { i } else { len - (i + digits) };
-                let numeric_text = &text[start..start+digits];
-                if let Some(digit) = numeric_text.to_digit() { return Some(digit) }
+                let numeric_text = &text[start..start + digits];
+                if let Some(digit) = numeric_text.to_digit() {
+                    return Some(digit);
+                }
             }
         }
     }
     None
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -59,7 +69,7 @@ mod tests {
 
     #[test]
     fn test_find_digits() {
-        assert_eq!(find_digit("two1nine"), 29);       
+        assert_eq!(find_digit("two1nine"), 29);
         assert_eq!(find_digit("eightwothree"), 83);
         assert_eq!(find_digit("abcone2threexyz"), 13);
         assert_eq!(find_digit("xtwone3four"), 24);
@@ -70,8 +80,8 @@ mod tests {
 
     #[test]
     fn test_sum_numbers_per_line() {
-        let sample = include_str!("./test_data/sample.txt"); 
-        assert_eq!(sum_numeric_calibration_nums(sample), 281);       
+        let sample = include_str!("./test_data/sample.txt");
+        assert_eq!(sum_numeric_calibration_nums(sample), 281);
 
         let input = include_str!("./test_data/input.txt");
         assert_eq!(sum_numeric_calibration_nums(input), 52834);
