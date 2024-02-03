4a8f6a032beb4c0181effd4c7e28c40a40a3dca0
Cheng Che <clementcheng56@gmail.com>
2024-02-03 21:42:13 +0800

    refactor(aoc23_day02/part_i): isolate the parse fn into game, game set, cube, ...etc.

--- days/aoc23_day02/src/bin/part_i/main.rs
+++ days/aoc23_day02/src/bin/part_i/main.rs
@@ -1,96 +1,109 @@
-#[derive(Debug)]
-struct Cube<'a> {
-    n_cubes: u32,
-    color: &'a str,
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+enum CubeColor {
+    Blue,
+    Green,
+    Red,
 }
 
-#[derive(Default, Debug)]
 struct CubePackage {
-    total_blue_cubes: u32,
-    total_green_cubes: u32,
-    total_red_cubes: u32,
+    cubes: HashMap<CubeColor, u32>,
 }
 
-#[derive(Debug)]
-struct Game<'a> {
-    package: &'a CubePackage,
+struct Cube {
+    quantity: u32,
+    color: CubeColor,
 }
 
-impl<'a> Game<'a> {
-    fn new(package: &'a CubePackage) -> Self {
-        Game { package }
+struct GameSet {
+    cubes: Vec<Cube>,
+}
+
+struct Game {
+    id: u32,
+    game_sets: Vec<GameSet>,
+}
+
+impl CubeColor {
+    fn from(s: &str) -> Option<Self> {
+        match s {
+            "blue" => Some(Self::Blue),
+            "green" => Some(Self::Green),
+            "red" => Some(Self::Red),
+            _ => None,
+        }
+    }
+}
+
+impl CubePackage {
+    fn new(total_blue_cubes: u32, total_green_cubes: u32, total_red_cubes: u32) -> Self {
+        let mut cubes = HashMap::new();
+        cubes.insert(CubeColor::Blue, total_blue_cubes);
+        cubes.insert(CubeColor::Green, total_green_cubes);
+        cubes.insert(CubeColor::Red, total_red_cubes);
+        Self { cubes }
     }
 
-    fn play(&mut self, game_box: &str) -> bool {
-        let game_sets = game_box
-            .split(";")
-            .map(|game_sets| game_sets.trim().split(","))
-            .map(|game_set| {
-                game_set
-                    .map(|cube_str| cube_str.trim().split_whitespace())
-                    .filter_map(|mut game_set| match (game_set.next(), game_set.next()) {
-                        (Some(n_cubes), Some(color)) if n_cubes.parse::<u32>().is_ok() => {
-                            Some(Cube {
-                                n_cubes: n_cubes.parse().unwrap(),
-                                color,
-                            })
-                        }
-                        _ => None,
-                    })
-                    .collect::<Vec<Cube>>()
-            })
-            .collect::<Vec<Vec<Cube>>>();
+    fn get(&self, color: &CubeColor) -> u32 {
+        *self.cubes.get(&color).unwrap_or(&0)
+    }
+}
 
-        for game_set in game_sets {
-            for cube in game_set {
-                let bad_game = match cube.color {
-                    "blue" => cube.n_cubes > self.package.total_blue_cubes,
-                    "green" => cube.n_cubes > self.package.total_green_cubes,
-                    "red" => cube.n_cubes > self.package.total_red_cubes,
-                    _ => true,
-                };
+impl Cube {
+    fn from(s: &str) -> Option<Self> {
+        let mut iter = s.trim().split_whitespace();
+        let quantity = iter.next()?.parse().ok()?;
+        let color = CubeColor::from(iter.next()?)?;
+        Some(Self { quantity, color })
+    }
+}
 
-                if bad_game {
-                    return false;
-                }
-            }
-        }
+impl GameSet {
+    fn from(s: &str) -> Option<Self> {
+        let cubes = s.split(',').filter_map(Cube::from).collect();
+        Some(Self { cubes })
+    }
 
-        true
+    fn is_valid(&self, package: &CubePackage) -> bool {
+        self.cubes
+            .iter()
+            .all(|cube| cube.quantity <= package.get(&cube.color))
+    }
+}
+
+impl Game {
+    fn new(game_box: &str) -> Option<Self> {
+        let mut iter = game_box.splitn(2, ": ");
+        let id = iter.next()?.trim_start_matches("Game ").parse().ok()?;
+        let game_sets = iter.next()?.split(';').filter_map(GameSet::from).collect();
+        Some(Self { id, game_sets })
+    }
+
+    fn is_valid(&self, package: &CubePackage) -> bool {
+        self.game_sets
+            .iter()
+            .all(|game_set| game_set.is_valid(package))
     }
 }
 
 fn play_game(game_box: &str, package: &CubePackage) -> u32 {
-    let game_prefix_len = "Game ".len();
-    let mut game = Game::new(package);
-    let good_game_id = game_box.lines().flat_map(|line| {
-        line.split_once(": ")
-            .filter(|&(_, game_box)| game.play(game_box))
-            .and_then(|(game_label, _)| {
-                let game_id = &game_label[game_prefix_len..];
-                game_id.parse::<u32>().ok()
-            })
-    });
-
-    good_game_id.sum::<u32>()
+    game_box
+        .lines()
+        .filter_map(|line| Game::new(line))
+        .filter(|game| game.is_valid(package))
+        .map(|game| game.id)
+        .sum()
 }
 
 fn main() {
     let game_box = include_str!("./test_data/sample.txt");
-    let package = &CubePackage {
-        total_blue_cubes: 14,
-        total_green_cubes: 13,
-        total_red_cubes: 12,
-    };
+    let package = &CubePackage::new(14, 13, 12);
     let answer = play_game(game_box, package);
     println!("[Sample] Sum of total success ID: {}", answer);
 
     let game_box = include_str!("./test_data/input.txt");
-    let package = &CubePackage {
-        total_blue_cubes: 14,
-        total_green_cubes: 13,
-        total_red_cubes: 12,
-    };
+    let package = &CubePackage::new(14, 13, 12);
     let answer = play_game(game_box, package);
     println!("Sum of total success ID: {}", answer);
 }
@@ -101,23 +114,16 @@ mod tests {
 
     #[test]
     fn test_single_round_game_set() {
-        let package = CubePackage {
-            total_blue_cubes: 2,
-            total_green_cubes: 2,
-            total_red_cubes: 2,
-        };
-        let mut game = Game::new(&package);
-        assert_eq!(game.play("1 blue, 1 green; 2 green, 2 blue, 2 red"), true);
-        assert_eq!(game.play("3 red"), false);
+        let package = CubePackage::new(2, 2, 2);
+        let game_set = GameSet::from("1 blue, 1 green; 2 green, 2 blue, 2 red");
+        assert_eq!(game_set.unwrap().is_valid(&package), true);
+        let game_set = GameSet::from("1 blue, 1 green; 2 green, 2 blue, 3 red");
+        assert_eq!(game_set.unwrap().is_valid(&package), false);
     }
 
     #[test]
     fn test_game_sets() {
-        let package = CubePackage {
-            total_blue_cubes: 14,
-            total_green_cubes: 13,
-            total_red_cubes: 12,
-        };
+        let package = &CubePackage::new(14, 13, 12);
         let game_box = include_str!("./test_data/sample.txt");
         assert_eq!(play_game(game_box, &package), 8);
 
